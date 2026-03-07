685e9f91d3c8eac655622cefafc6451d5c721af8
Samuel Machado <sam.machado16@gmail.com>
2026-03-07 20:42:13 -0300

    a

--- páginas/script.js
+++ páginas/script.js
@@ -1,5 +1,3 @@
-var add_inc = document.querySelector('input#add-inc')
-var add_alo = document.querySelector('input#add-ali')
 var res = document.querySelector('p#teste')
 
 function AddInciso() {
@@ -8,6 +6,13 @@ function AddInciso() {
     i.setAttribute('id', 'inc')
     inciso.innerHTML = 'Inciso <br>'
     inciso.appendChild(i)
+    inciso.innerHTML += '<br>'
+
+    let add_inc = document.createElement('input')
+    add_inc.setAttribute('type', 'submit')
+    add_inc.setAttribute('id', 'add-inc')
+    add_inc.setAttribute('value', 'Adicionar')
+    inciso.appendChild(add_inc)
 }
 
 function AddAlinea() {
@@ -16,4 +21,11 @@ function AddAlinea() {
     a.setAttribute('id', 'ali')
     alinea.innerHTML = 'Alínea <br>'
     alinea.appendChild(a)
+    alinea.innerHTML += '<br>'
+
+    let add_ali = document.createElement('input')
+    add_ali.setAttribute('type', 'submit')
+    add_ali.setAttribute('id', 'add-ali')
+    add_ali.setAttribute('value', 'Adicionar')
+    alinea.appendChild(add_ali)
 }
